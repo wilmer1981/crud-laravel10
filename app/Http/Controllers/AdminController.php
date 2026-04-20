162ca2f17dd5564969c4af9ad34d7c4b5214fde7
Wilmer Saldaña <wilmer_1981@hotmail.com>
2026-04-20 00:54:20 -0500

    se agregó el index

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     //
     public function index(){
-        return view('admin.index');
+        $usuarios = User::all();
+        return view('admin.index',['usuarios'=>$usuarios]);
     }
 
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,4 +4,30 @@
  <div class="row">
     <h1>Principal</h1>
  </div>
+<div class="row">
+    <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+               @php $cont_user = 0; @endphp
+
+               @foreach($usuarios as $usuario)
+                  @php $cont_user++; @endphp
+               @endforeach
+                <h3>{{$cont_user}}</h3>
+
+                <p>Usuarios Registrados</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="{{url('/admin/usuarios')}}" class="small-box-footer">
+                Más información <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+
+</div>
+
+
 @endsection
